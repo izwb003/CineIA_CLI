079684a22a902dd5838fcf9d3f0a231ae2ecd714
songd <izwb003@gmail.com>
2024-01-26 15:39:33 +0800

    Finished test.cpp

--- test.cpp
+++ test.cpp
@@ -4,12 +4,15 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 
 #include "cineia.h"
 
 using namespace SMPTE::ImmersiveAudioBitstream;
 using namespace std;
 
+/*
 int main() {
     ifstream* imfInput = new ifstream("2.iab", ios::binary);
     ofstream* atmosOutput = new ofstream("2.atmos", ios::binary);
@@ -31,4 +34,38 @@ int main() {
     delete atmosOutput;
     atmosOutput = nullptr;
     return 0;
+}
+ */
+
+std::string IntToStringWithLeadingZeros(int value, int width) {
+    std::stringstream ss;
+    ss << std::setw(width) << std::setfill('0') << value;
+    return ss.str();
+}
+
+int main() {
+    for(int frameNum = 0; ; frameNum ++) {
+        string imfFileName = "IMFIAB/" + to_string(frameNum) + ".iab";
+        ifstream* imfInput = new ifstream(imfFileName, ios::binary);
+        if(!imfInput->is_open()) return 0;
+        string atmosFileName = "DCPIAB/" + IntToStringWithLeadingZeros(frameNum, 6) + ".atmos";
+        cout<<atmosFileName<<endl;
+        ofstream* atmosOutput = new ofstream(atmosFileName, ios::out | ios::binary);
+        // Make output file buffer
+        vector<char> atmosBuffer;
+        uint32_t atmosFileSize = 0;
+        iabError error;
+        // Make conversion
+        error = reassembleIAB(imfInput, atmosBuffer, atmosFileSize);
+        // Copy PreambleValue
+        copyPreambleValue(imfInput, atmosBuffer, atmosFileSize);
+        // Write output
+        atmosOutput->write(atmosBuffer.data(), atmosFileSize);
+        imfInput->close();
+        atmosOutput->close();
+        delete imfInput;
+        imfInput = nullptr;
+        delete atmosOutput;
+        atmosOutput = nullptr;
+    }
 }
